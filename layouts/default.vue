494c232f9b473b5236704f2a639e215470a976c1
Jenesix <acearcadia789@gmail.com>
2022-06-27 02:25:19 +0700

    NavBar New ver.

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,36 +37,20 @@
           >mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon
         >
       </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon class="yellow--text text--darken-3">mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon class="yellow--text text--darken-3">mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon class="yellow--text text--darken-3">mdi-menu</v-icon>
-      </v-btn>
+      <v-app-bar-title>Title</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+      <div>
+        <v-btn text> Test1 </v-btn>
+
+        <v-btn text> Test2 </v-btn>
+
+        <v-btn text> Test3 </v-btn>
+        <v-btn text> Test4 </v-btn>
+        <v-btn text> Test5 </v-btn>
+      </div>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <Nuxt />
   </v-app>
 </template>
 
@@ -91,14 +75,14 @@ export default {
         },
         {
           icon: 'mdi-apps',
-          title: 'slider',
+          title: 'Slider',
           to: '/slider',
         },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'TSM_Academy',
     }
   },
 }
