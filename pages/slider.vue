aa093cb59c2759397d6119f5610a4f1dd70e5639
Jenesix <acearcadia789@gmail.com>
2022-06-27 23:35:19 +0700

    หน้า Slider เพิ่มปุ่ม

--- pages/slider.vue
+++ pages/slider.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-carousel class="mt-5">
+    <v-carousel class="mt-12 pt-12">
       <v-carousel-item
         v-for="(item, i) in items"
         :key="i"
@@ -10,6 +10,24 @@
       >
       </v-carousel-item>
     </v-carousel>
+    <v-container>
+      <div>
+        <v-row justify="center" align="center">
+          <v-col cols="11" class="mt-8"
+            ><v-btn class="mr-10" height="40%" width="20%"
+              ><v-card height="200px"></v-card>
+            </v-btn>
+            <v-btn class="ma-10" height="40%" width="20%"
+              ><v-card height="200px"></v-card>
+            </v-btn>
+            <v-btn class="ma-10" height="40%" width="20%"
+              ><v-card height="200px"></v-card>
+            </v-btn>
+            <v-btn class="ma-10" height="40%" width="20%"
+              ><v-card height="200px"></v-card> </v-btn></v-col
+        ></v-row>
+      </div>
+    </v-container>
   </v-container>
 </template>
 
